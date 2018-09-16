01535c240eca5b5f88ebb01245e7ed3b0ad1abf1
Mahesh Natarajan <umamags@gmail.com>
2018-09-16 10:33:08 +0530

    routingdone

--- angular5/angular-tour-of-heroes/src/app/hero.service.ts
+++ angular5/angular-tour-of-heroes/src/app/hero.service.ts
@@ -2,7 +2,7 @@ import {Injectable} from '@angular/core';
 import {Hero} from './hero';
 import {HEROES} from './mock-heroes';
 import {Observable, of} from 'rxjs';
-import { MessageService } from './message.service';
+import {MessageService} from './message.service';
 
 @Injectable({
   providedIn: 'root'
@@ -13,5 +13,11 @@ export class HeroService {
     return of(HEROES);
   }
 
-  constructor(private messageService: MessageService) { }
+  constructor(private messageService: MessageService) {}
+
+  getHero(id: number): Observable<Hero> {
+    // TODO: send the message _after_ fetching the hero
+    this.messageService.add(`HeroService: fetched hero id=${id}`);
+    return of(HEROES.find(hero => hero.id === id));
+  }
 }
--- angular5/angular-tour-of-heroes/src/app/heroes/heroes.component.ts
+++ angular5/angular-tour-of-heroes/src/app/heroes/heroes.component.ts
@@ -9,7 +9,6 @@ import {HeroService} from '../hero.service';
 })
 export class HeroesComponent implements OnInit {
 
-  selectedHero: Hero;
   heroes: Hero[];
 
   constructor(private heroService: HeroService) {}
@@ -18,9 +17,6 @@ export class HeroesComponent implements OnInit {
     this.getHeroes();
   }
 
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
-  }
   getHeroes(): void {
     this.heroService.getHeroes()
       .subscribe(heroes => this.heroes = heroes);
