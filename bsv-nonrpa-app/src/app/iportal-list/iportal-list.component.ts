9b322095f7b15616e40c26cdf5c1824493f4f944
Mahesh Natarajan <umamags@gmail.com>
2020-05-23 20:12:38 +0530

    routes works

--- bsv-nonrpa-app/src/app/iportal-list/iportal-list.component.ts
+++ bsv-nonrpa-app/src/app/iportal-list/iportal-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-iportal-list',
@@ -7,9 +8,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class IportalListComponent implements OnInit {
 
-  constructor() { }
+  columnDefs = [
+        {headerName: 'Make', field: 'make', sortable: true, filter: true },
+        {headerName: 'Model', field: 'model', sortable: true, filter: true },
+        {headerName: 'Price', field: 'price', sortable: true, filter: true}
+    ];
+	
+	rowData: any;
+	constructor(private http: HttpClient) {
 
-  ngOnInit(): void {
-  }
+    }	
+	ngOnInit() {
+        this.rowData = this.http.get('https://raw.githubusercontent.com/ag-grid/ag-grid/master/grid-packages/ag-grid-docs/src/sample-data/smallRowData.json');
+    }
 
 }
