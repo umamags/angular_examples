32c88bfd411e45555a033b8a7e2396cf9d8f46e5
Mahesh Natarajan <umamags@gmail.com>
2020-05-23 19:56:39 +0530

    routes work

--- bsv-nonrpa-app/src/app/app.module.ts
+++ bsv-nonrpa-app/src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppComponent } from './app.component';
 import { AgGridModule } from 'ag-grid-angular';
 import { HttpClientModule } from '@angular/common/http';
 import { IportalListComponent } from './iportal-list/iportal-list.component';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { IportalListComponent } from './iportal-list/iportal-list.component';
   imports: [
     BrowserModule,
     HttpClientModule,
-    AgGridModule.withComponents([])
+    AgGridModule.withComponents([]),
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
